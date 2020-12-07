02a90eb6b74fec0c9b2107a3ff0c05bdc4f33fb8
Vincent van Hoef <vvanhoef@hotmail.com>
2020-12-07 18:25:34 +0100

    sed fix

--- sphinx_update.sh
+++ sphinx_update.sh
@@ -4,7 +4,7 @@ echo "" >> docs/conf.py
 echo "master_doc = 'index'" >> docs/conf.py
 echo "numfig = True" >> docs/conf.py
 
-sed -i 's/alabaster/sphinx_rtd_theme/g' docs/conf.py
+sed -i '' 's/alabaster/sphinx_rtd_theme/g' docs/conf.py
 
 mkdir docs/content
 
